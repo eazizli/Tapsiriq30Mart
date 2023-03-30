17cbeb5eeba64018557f3308fd22cfd070d13e7e
Tahir <tahirhsnli5@gmail.com>
2023-03-30 16:42:43 +0400

    .

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+       
+    //int[] arr = { -1, 2, -3, 4, -5 };
+    //    Type(ref arr);
+    //    foreach (int i in arr)
+    //    {
+    //        Console.WriteLine(i);
+    //    }
+    //    int[] arr = { 1, 2, 3, 4, 5 };
+    //    insertArray( ref arr,12);
+    //    Console.WriteLine(arr[arr.Length-1]);   
+
+    }
+
+    //static void insertArray( ref int[] arr,int num)
+    //{
+    //    int [] array = new int[arr.Length+1];
+    //    for(int i = 0; i < arr.Length; i++)
+    //    {
+    //        array[i] = arr[i];
+
+    //    }
+    //    array[array.Length-1] = num;
+    //    arr = array;    
+    //}
+
+    //static void Type( ref int[]arr)
+    //{
+    //    int[] arr1 = arr;
+    //    for(int i = 0; i < arr.Length; i++)
+    //    {
+    //        if (arr[i] < 0)
+    //        {
+    //            arr[i] *= -1;
+    //        }
+    //        else
+    //        {
+    //            arr1[i] *= 1;
+    //        }
+    //    }   
+    //}
+}
